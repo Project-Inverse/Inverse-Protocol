dbd391ffdbedff9cb40ab40e251d88d94a7320e4
ravneet <ravneet@antech.in>
2021-01-22 18:42:22 +0530

    swap and staking updated

--- XIV.sol
+++ XIV.sol
@@ -1,14 +1,13 @@
 /**
  *Submitted for verification at Etherscan.io on 2019-04-26
-*/
+ */
 
 pragma solidity 0.5.7;
 
 library SafeMath {
-
     /**
-    * @dev Multiplies two numbers, throws on overflow.
-    */
+     * @dev Multiplies two numbers, throws on overflow.
+     */
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
         if (a == 0) {
             return 0;
@@ -19,8 +18,8 @@ library SafeMath {
     }
 
     /**
-    * @dev Integer division of two numbers, truncating the quotient.
-    */
+     * @dev Integer division of two numbers, truncating the quotient.
+     */
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
         // assert(b > 0); // Solidity automatically throws when dividing by 0
         uint256 c = a / b;
@@ -29,16 +28,16 @@ library SafeMath {
     }
 
     /**
-    * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-    */
+     * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+     */
     function sub(uint256 a, uint256 b) internal pure returns (uint256) {
         assert(b <= a);
         return a - b;
     }
 
     /**
-    * @dev Adds two numbers, throws on overflow.
-    */
+     * @dev Adds two numbers, throws on overflow.
+     */
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
         uint256 c = a + b;
         assert(c >= a);
@@ -47,17 +46,19 @@ library SafeMath {
 }
 
 contract Ownable {
-
     address public owner;
     bool public stopped = false;
 
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
 
     /**
      * @dev The Ownable constructor sets the original `owner` of the contract to the sender
      * account.
      */
-    constructor() public{
+    constructor() public {
         owner = msg.sender;
     }
 
@@ -79,17 +80,17 @@ contract Ownable {
         owner = newOwner;
     }
 
-    /** 
-    * Stop ICO/Contract
-    */
-    function stop() onlyOwner public{
+    /**
+     * Stop ICO/Contract
+     */
+    function stop() public onlyOwner {
         stopped = true;
     }
 
-    /** 
-    * Start ICO/Contract
-    */
-    function start() onlyOwner public{
+    /**
+     * Start ICO/Contract
+     */
+    function start() public onlyOwner {
         stopped = false;
     }
 
@@ -97,36 +98,38 @@ contract Ownable {
     Validate if ICO/Contract running
     */
     modifier isRunning {
-        assert (!stopped);
+        assert(!stopped);
         _;
     }
 }
 
 contract ERC20Basic {
     function totalSupply() public view returns (uint256);
+
     function balanceOf(address who) public view returns (uint256);
+
     function transfer(address to, uint256 value) public returns (bool);
+
     event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
 contract BasicToken is ERC20Basic {
-
     using SafeMath for uint256;
     mapping(address => uint256) balances;
     uint256 totalSupply_;
 
     /**
-    * @dev total number of tokens in existence
-    */
+     * @dev total number of tokens in existence
+     */
     function totalSupply() public view returns (uint256) {
         return totalSupply_;
     }
 
     /**
-    * @dev transfer token for a specified address
-    * @param _to The address to transfer to.
-    * @param _value The amount to be transferred.
-    */
+     * @dev transfer token for a specified address
+     * @param _to The address to transfer to.
+     * @param _value The amount to be transferred.
+     */
     function transfer(address _to, uint256 _value) public returns (bool) {
         require(_to != address(0));
         require(_value <= balances[msg.sender]);
@@ -139,39 +142,37 @@ contract BasicToken is ERC20Basic {
     }
 
     /**
-    * @dev Gets the balance of the specified address.
-    * @param _owner The address to query the the balance of.
-    * @return An uint256 representing the amount owned by the passed address.
-    */
+     * @dev Gets the balance of the specified address.
+     * @param _owner The address to query the the balance of.
+     * @return An uint256 representing the amount owned by the passed address.
+     */
     function balanceOf(address _owner) public view returns (uint256 balance) {
         return balances[_owner];
     }
 
     /**
-   * @dev Internal function that mints an amount of the token and assigns it to
-   * an account. This encapsulates the modification of balances such that the
-   * proper events are emitted.
-   * @param account The account that will receive the created tokens.
-   * @param value The amount that will be created.
-   */
-  function _mint(address account, uint256 value) internal {
-    require(account != address(0));
-    totalSupply_ = totalSupply_.add(value);
-    balances[account] = balances[account].add(value);
-    emit Transfer(address(0), account, value);
-  }
-
+     * @dev Internal function that mints an amount of the token and assigns it to
+     * an account. This encapsulates the modification of balances such that the
+     * proper events are emitted.
+     * @param account The account that will receive the created tokens.
+     * @param value The amount that will be created.
+     */
+    function _mint(address account, uint256 value) internal {
+        require(account != address(0));
+        totalSupply_ = totalSupply_.add(value);
+        balances[account] = balances[account].add(value);
+        emit Transfer(address(0), account, value);
+    }
 }
 
 contract BurnableToken is BasicToken, Ownable {
-
     event Burn(address indexed burner, uint256 value);
 
     /**
      * @dev Burns a specific amount of tokens.
      * @param _value The amount of token to be burned.
      */
-    function burn(uint256 _value) public onlyOwner{
+    function burn(uint256 _value) public onlyOwner {
         require(_value <= balances[msg.sender]);
         // no need to require value <= totalSupply, since that would imply the
         // sender's balance is greater than the totalSupply, which *should* be an assertion failure
@@ -184,28 +185,55 @@ contract BurnableToken is BasicToken, Ownable {
 }
 
 contract ERC20 is ERC20Basic {
-    function allowance(address owner, address spender) public view returns (uint256);
-    function transferFrom(address from, address to, uint256 value) public returns (bool);
+    function allowance(address owner, address spender)
+        public
+        view
+        returns (uint256);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) public returns (bool);
+
     function approve(address spender, uint256 value) public returns (bool);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
 }
 
 library SafeERC20 {
-
-    function safeTransfer(ERC20Basic token, address to, uint256 value) internal {
+    function safeTransfer(
+        ERC20Basic token,
+        address to,
+        uint256 value
+    ) internal {
         assert(token.transfer(to, value));
     }
-    function safeTransferFrom(ERC20 token, address from, address to, uint256 value) internal {
+
+    function safeTransferFrom(
+        ERC20 token,
+        address from,
+        address to,
+        uint256 value
+    ) internal {
         assert(token.transferFrom(from, to, value));
     }
-    function safeApprove(ERC20 token, address spender, uint256 value) internal {
+
+    function safeApprove(
+        ERC20 token,
+        address spender,
+        uint256 value
+    ) internal {
         assert(token.approve(spender, value));
     }
 }
 
 contract StandardToken is ERC20, BasicToken {
-
-    mapping (address => mapping (address => uint256)) internal allowed;
+    mapping(address => mapping(address => uint256)) internal allowed;
 
     /**
      * @dev Transfer tokens from one address to another
@@ -213,7 +241,11 @@ contract StandardToken is ERC20, BasicToken {
      * @param _to address The address which you want to transfer to
      * @param _value uint256 the amount of tokens to be transferred
      */
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool) {
         require(_to != address(0));
         require(_value <= balances[_from]);
         require(_value <= allowed[_from][msg.sender]);
@@ -247,7 +279,11 @@ contract StandardToken is ERC20, BasicToken {
      * @param _spender address The address which will spend the funds.
      * @return A uint256 specifying the amount of tokens still available for the spender.
      */
-    function allowance(address _owner, address _spender) public view returns (uint256) {
+    function allowance(address _owner, address _spender)
+        public
+        view
+        returns (uint256)
+    {
         return allowed[_owner][_spender];
     }
 
@@ -261,8 +297,13 @@ contract StandardToken is ERC20, BasicToken {
      * @param _spender The address which will spend the funds.
      * @param _addedValue The amount of tokens to increase the allowance by.
      */
-    function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
-        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+    function increaseApproval(address _spender, uint256 _addedValue)
+        public
+        returns (bool)
+    {
+        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(
+            _addedValue
+        );
         emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
         return true;
     }
@@ -277,8 +318,11 @@ contract StandardToken is ERC20, BasicToken {
      * @param _spender The address which will spend the funds.
      * @param _subtractedValue The amount of tokens to decrease the allowance by.
      */
-    function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
-        uint oldValue = allowed[msg.sender][_spender];
+    function decreaseApproval(address _spender, uint256 _subtractedValue)
+        public
+        returns (bool)
+    {
+        uint256 oldValue = allowed[msg.sender][_spender];
         if (_subtractedValue > oldValue) {
             allowed[msg.sender][_spender] = 0;
         } else {
@@ -300,43 +344,52 @@ contract ERC20Mintable is BasicToken, Ownable {
      * @param value The amount of tokens to mint.
      * @return A boolean that indicates if the operation was successful.
      */
-    function mint( address to, uint256 value ) public onlyOwner returns (bool){
-      _mint(to, value);
-      return true;
+    function mint(address to, uint256 value) public onlyOwner returns (bool) {
+        _mint(to, value);
+        return true;
     }
-  }
+}
 
 contract XIV is StandardToken, BurnableToken, ERC20Mintable {
+    using SafeMath for uint256;
 
-    using SafeMath for uint;
+    string public constant symbol = "XIV";
+    string public constant name = "XIV";
 
-    string constant public symbol = "XIV";
-    string constant public name = "XIV";
-
-    uint8 constant public decimals = 18;
-    uint256 public constant decimalFactor = 10 ** uint256(decimals);
+    uint8 public constant decimals = 18;
+    uint256 public constant decimalFactor = 10**uint256(decimals);
     uint256 public constant INITIAL_SUPPLY = 0 * decimalFactor;
 
     constructor(address ownerAdrs) public {
         totalSupply_ = INITIAL_SUPPLY;
         //InitialDistribution
-        preSale(ownerAdrs,totalSupply_);
+        preSale(ownerAdrs, totalSupply_);
     }
 
-    function preSale(address _address, uint _amount) internal returns (bool) {
+    function preSale(address _address, uint256 _amount)
+        internal
+        returns (bool)
+    {
         balances[_address] = _amount;
         emit Transfer(address(0x0), _address, _amount);
     }
 
-    
-    function transfer(address _to, uint256 _value) isRunning public returns (bool) {
+    function transfer(address _to, uint256 _value)
+        public
+        isRunning
+        returns (bool)
+    {
         super.transfer(_to, _value);
         return true;
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) isRunning public returns (bool) {
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public isRunning returns (bool) {
         super.transferFrom(_from, _to, _value);
         return true;
     }
-    
 }
+
--- XIVStaking.sol
+++ XIVStaking.sol
@@ -1,46 +1,9 @@
 // SPDX-License-Identifier: UNLICENCED
-pragma solidity 0.8.0;
-library SafeMath {
+pragma solidity >=0.6.0 <0.8.0;
 
-    /**
-    * @dev Multiplies two numbers, throws on overflow.
-    */
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        if (a == 0) {
-            return 0;
-        }
-        uint256 c = a * b;
-        assert(c / a == b);
-        return c;
-    }
+import "./Ownable.sol";
+import "./SafeMath.sol";
 
-    /**
-    * @dev Integer division of two numbers, truncating the quotient.
-    */
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        // assert(b > 0); // Solidity automatically throws when dividing by 0
-        uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-        return c;
-    }
-
-    /**
-    * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-    */
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        assert(b <= a);
-        return a - b;
-    }
-
-    /**
-    * @dev Adds two numbers, throws on overflow.
-    */
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        assert(c >= a);
-        return c;
-    }
-}
 interface Token{
     function transferOwnership(address newOwner) external;
     function stop() external;
@@ -60,50 +23,226 @@ interface Token{
     function burn(uint256 _value) external;
     function burnTokens(address who,uint256 _value) external;
 }
-contract XIVStaking{
+
+interface OracleInterface{
+    function latestAnswer() external view returns (int256);
+}
+
+contract XIVStaking is Ownable{
+    
     using SafeMath for uint256;
-    uint256 XIVPrice; //in wei, 18 decimals
-    address owner; //contract owner
-    address XIVTokenContractAddress = 0xd9145CCE52D386f254917e481eB44e9943F39138; //XIV contract address
-    constructor(){
-        owner = msg.sender;
+    uint256 XIVPrice=1000000000000000000; //in wei, 18 decimals
+    uint256 XIVPriceInUSDT=100000000; // in tokens with decimals 
+    address XIVTokenContractAddress = 0xe667ee780908cAf92De34d7a749d9ACC1FB702C6; //XIV contract address
+    address USDTContractAddress = 0xBbf126a88DE8c993BFe67c46Bb333a2eC71bC3fF; //USDT contract address
+    mapping (address=>uint256) public tokensStaked; //amount of XIV staked by user
+    address public ethOracleAddress=0x8A753747A1Fa494EC906cE90E9f37563A8AF630e;
+    address oracleWrapperContractAddress = 0x8EAb4Ee4C4c4619Fe48C87248369E44380B9E2BF; //address of oracle wrapper from where the prices would be fetched
+    
+    struct DefiCoin{
+        uint16 oracleType;
+        bool status;
     }
-    function getXIVPrice() public view returns(uint256){
-        return XIVPrice;
+    
+    mapping(address=>DefiCoin) public defiCoinsForFlexibleMapping;
+    address[] public allFlexibleContractAddressArray;
+    mapping(address=>DefiCoin) public defiCoinsForFixedMapping;
+    address[] public allFixedContractAddressArray;
+    
+    struct IndexCoin{
+        uint16 oracleType;
+        bool status;
+        uint256 contributionPercentage; //10**4
     }
-    function setXIVPrice(uint256 price) public onlyOwner{
-       XIVPrice = price; 
+    mapping(address=>IndexCoin) public defiCoinsForFlexibleIndexMapping;
+    address[] public allIndexFlexibleContractAddressArray;
+    mapping(address=>IndexCoin) public defiCoinsForFixedIndexMapping;
+    address[] public allIndexFixedContractAddressArray;
+    
+    uint256 betid;
+    struct BetInfo{
+        uint256 id;
+        uint256 amount;
+        address contractAddress;
+        uint256 betType;
+        uint256 betMarketCapAmount;
+        uint256 timestamp;
+        uint256 status; // 0->bet active, 1->bet won, 2->bet lost
     }
-     modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
+    BetInfo[] public betArray;
+    mapping(uint256=>uint256) public findBetInArrayUsingBetIdMapping;
+    
+    function getXIVPrice(uint16 typeOfPrice) public view returns(uint256){
+        //typeOfPrice =0 for eth and 1 for USDT
+        //TODO fetch price from price oracle
+        return typeOfPrice == 0 ? XIVPrice : XIVPriceInUSDT;
     }
-    function calculateAmtOfTokens(uint256 amountInWei) public view returns(uint256){
-        //returns tokens
-       return (10**Token(XIVTokenContractAddress).decimals()*(amountInWei)).div(XIVPrice);
-    }
-    function calculateETHTobeGiven(uint256 XIVtokensTobeExchanged) public view returns(uint256){
-        //returns wei
-        return (XIVtokensTobeExchanged.mul(XIVPrice)).div(10**Token(XIVTokenContractAddress).decimals());
-    }
-    function buyTokens() public payable{
-        Token tokenObj = Token(XIVTokenContractAddress);
-        require(msg.value !=0, "Please send ETH to purchase XIV");
-        require(tokenObj.balanceOf(address(this))> calculateAmtOfTokens(msg.value) , 
-        "XIV Tokens not available");
-        tokenObj.transfer(msg.sender,calculateAmtOfTokens(msg.value));
+    function setXIVPrice(uint256 price, uint16 typeOfPrice) public onlyOwner{
+        //TODO to be removed when XIV price available in price oracle
+        if(typeOfPrice == 0){
+            XIVPrice = price;
+        }else{
+            XIVPriceInUSDT=price;
+        }
         
     }
-    function sellTokens(uint256 amountOfTokensToBeSold) public{
-        Token tokenObj = Token(XIVTokenContractAddress);
-        //check if user has balance
-        require(tokenObj.balanceOf(msg.sender) >= amountOfTokensToBeSold, "You dont have enough XIV balance");
-        //check if user has provided allowance
-        require(tokenObj.allowance(msg.sender,address(this)) >= amountOfTokensToBeSold, 
-        "Please allow smart contract to spend on your behalf");
-        //ETH balance should be greater than  equal to required
-        require(address(this).balance <= calculateETHTobeGiven(amountOfTokensToBeSold)); 
-        tokenObj.transferFrom(msg.sender,address(this),amountOfTokensToBeSold);
-        payable(msg.sender).transfer(calculateETHTobeGiven(amountOfTokensToBeSold));
+    function calculateAmtOfTokens(uint256 amount, uint256 typeOfPrice) public view returns(uint256){
+        //returns tokens
+        //0-ETH and 1 for USDT
+        if(typeOfPrice == 0){
+            return (10**Token(XIVTokenContractAddress).decimals()*(amount)).div(XIVPrice);
+        }else{
+            return (10**Token(XIVTokenContractAddress).decimals()*(amount)).div(XIVPriceInUSDT);
+        }
     }
+    function calculateETHUSDTTobeGiven(uint256 XIVtokensTobeExchanged, uint256 typeOfPrice) public view returns(uint256){
+        //returns wei
+        if(typeOfPrice == 0){
+            return (XIVtokensTobeExchanged.mul(XIVPrice)).div(10**Token(XIVTokenContractAddress).decimals());
+        }else{
+            return (XIVtokensTobeExchanged.mul(XIVPriceInUSDT)).div(10**Token(XIVTokenContractAddress).decimals());
+        }
+        
+    }
+    function buyTokens(uint256 typeOfBuy, uint256 amount) public payable{
+        //typeOfBuy ==0 ETH and 1 for USDT
+        if(typeOfBuy == 0){
+            Token tokenObj = Token(XIVTokenContractAddress);
+            require(msg.value !=0, "Please send ETH to purchase XIV");
+            require(tokenObj.balanceOf(address(this))> calculateAmtOfTokens(msg.value,0) , 
+            "XIV Tokens not available");
+            tokenObj.transfer(msg.sender,calculateAmtOfTokens(msg.value,0));
+        }else{
+            Token tokenObj = Token(XIVTokenContractAddress);
+            Token usdtTokenObj = Token(USDTContractAddress);
+            checkTokens(amount);
+            require(tokenObj.balanceOf(address(this))> calculateAmtOfTokens(amount,1) , 
+            "XIV Tokens not available");
+            usdtTokenObj.transferFrom(msg.sender,address(this),amount);
+            tokenObj.transfer(msg.sender,calculateAmtOfTokens(amount,1));
+        }
+    }
+    function sellTokens(uint256 amountOfTokensToBeSold, uint256 typeOfCurrencyTobeReturned) public{
+        //typeOfCurrencyTobeReturned  == 0 for ETH and 1 for USDT
+        checkTokens(amountOfTokensToBeSold);
+        Token tokenObj = Token(XIVTokenContractAddress);
+        //ETH balance should be greater than  equal to required
+        if(typeOfCurrencyTobeReturned == 0){
+            require(address(this).balance >= calculateETHUSDTTobeGiven(amountOfTokensToBeSold,0),"The Contract does not have enough ethers."); 
+            tokenObj.transferFrom(msg.sender,address(this),amountOfTokensToBeSold);
+            payable(msg.sender).transfer(calculateETHUSDTTobeGiven(amountOfTokensToBeSold,0)); 
+        }else{
+            Token usdtTokenObj = Token(USDTContractAddress);
+            require(usdtTokenObj.balanceOf(address(this)) >= calculateETHUSDTTobeGiven(amountOfTokensToBeSold,1),"The Contract does not have enough USDT tokens."); 
+            tokenObj.transferFrom(msg.sender,address(this),amountOfTokensToBeSold);
+            usdtTokenObj.transfer(msg.sender,calculateETHUSDTTobeGiven(amountOfTokensToBeSold,1));
+        }
+        
+    }
+    function stakeTokens(uint256 amount) public{
+        checkTokens(amount);
+        Token tokenObj = Token(XIVTokenContractAddress);
+        tokenObj.transferFrom(msg.sender,address(this),amount);
+        tokensStaked[msg.sender] = tokensStaked[msg.sender].add(amount);
+    }
+     function unStakeTokens(uint256 amount) public{
+        require(tokensStaked[msg.sender]>=amount, "You don't have enough staking balance");
+        Token tokenObj = Token(XIVTokenContractAddress);
+        require(tokenObj.balanceOf(address(this)) >= amount, "You don't have enough XIV balance");
+        //check if user has provided allowance
+        tokenObj.transfer(msg.sender,amount);
+        tokensStaked[msg.sender] = tokensStaked[msg.sender].sub(amount);
+    }
+    
+    
+    function betForDefi(uint256 amountOfXIV, uint256 typeOfBet, address _betContractAddress) public{
+        // 0-> defi Fixed, 1->defi flexible, 2-> index Fixed and 3-> index flexible
+        checkTokens(amountOfXIV);
+        if(typeOfBet==0){
+            // defi fixed
+            
+        }else if(typeOfBet==1){
+            //defi flexible
+            
+        }else if(typeOfBet==2){
+            //index Fixed 
+            defiCoinsForFixedIndexMapping[_betContractAddress];
+            
+        }else if(typeOfBet==3){
+            //index flexible
+            
+        }
+    }
+    
+    function addUpdateForDefiCoinFlexible(address _ContractAddress, uint16 _OracleType, bool _Status) public onlyOwner{
+        // add update defi felxible coin
+        DefiCoin memory dCoin=DefiCoin({
+            oracleType:_OracleType,
+            status:_Status
+        });
+        defiCoinsForFlexibleMapping[_ContractAddress]=dCoin;
+        // check wheather contract exists in allFlexibleContractAddressArray array
+        if(!contractAvailableInArray(_ContractAddress,allFlexibleContractAddressArray)){
+            allFlexibleContractAddressArray.push(_ContractAddress);
+        }
+    }
+    function addUpdateForDefiCoinFixed(address _ContractAddress, uint16 _OracleType, bool _Status) public onlyOwner{
+        // add update defi fixed coin
+        DefiCoin memory dCoin=DefiCoin({
+            oracleType:_OracleType,
+            status:_Status
+        });
+        defiCoinsForFixedMapping[_ContractAddress]=dCoin;
+        // check wheather contract exists in allFixedContractAddressArray array
+        if(!contractAvailableInArray(_ContractAddress,allFixedContractAddressArray)){
+            allFixedContractAddressArray.push(_ContractAddress);
+        }
+    }
+     function addUpdateForIndexCoinFlexible(address _ContractAddress, uint16 _OracleType, bool _Status, uint256 _contributionPercentage) public onlyOwner{
+        // add update index fixed coin
+       IndexCoin memory iCoin=IndexCoin({
+            oracleType:_OracleType,
+            status:_Status,
+            contributionPercentage:_contributionPercentage
+        });
+        defiCoinsForFlexibleIndexMapping[_ContractAddress]=iCoin;
+        // check wheather contract exists in allFixedContractAddressArray array
+        if(!contractAvailableInArray(_ContractAddress,allIndexFlexibleContractAddressArray)){
+            allIndexFlexibleContractAddressArray.push(_ContractAddress);
+        }
+    }
+     function addUpdateForIndexCoinFixed(address _ContractAddress, uint16 _OracleType, 
+                                            bool _Status, uint256 _contributionPercentage) public onlyOwner{
+        // add update index fixed coin
+        IndexCoin memory iCoin=IndexCoin({
+            oracleType:_OracleType,
+            status:_Status,
+            contributionPercentage:_contributionPercentage
+        });
+        defiCoinsForFixedIndexMapping[_ContractAddress]=iCoin;
+        // check wheather contract exists in allFixedContractAddressArray array
+        if(!contractAvailableInArray(_ContractAddress,allIndexFixedContractAddressArray)){
+            allIndexFixedContractAddressArray.push(_ContractAddress);
+        }
+    }
+   
+    function contractAvailableInArray(address _ContractAddress,address[] memory _contractArray) internal pure returns(bool){
+        for(uint256 i=0;i<_contractArray.length;i++){
+            if(_ContractAddress==_contractArray[i]){
+                return true;
+            }
+        }
+        return false;
+    }  
+    
+    function checkTokens(uint256 amountOfXIV) internal view returns(bool){
+         Token tokenObj = Token(XIVTokenContractAddress);
+        //check if user has balance
+        require(tokenObj.balanceOf(msg.sender) >= amountOfXIV, "You don't have enough XIV balance");
+        //check if user has provided allowance
+        require(tokenObj.allowance(msg.sender,address(this)) >= amountOfXIV, 
+        "Please allow smart contract to spend on your behalf");
+        return true;
+    }
+   
 }
